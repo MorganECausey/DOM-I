158da87b0218e57c2f2eb0b205bcddf1e91081fa
Morgan Causey <morganelizabethcausey@gmail.com>
2023-07-27 16:35:56 -0400

    last update

--- src/index.js
+++ src/index.js
@@ -39,4 +39,5 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
   },
 };
 
-console.log('project wired!')
+const logoImg = document.querySelector("#logo-img")
+logoImg.src = siteContent.images['logo-img']
