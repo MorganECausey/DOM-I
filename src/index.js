3e0928789b59bf7f5d9fcdfcf63f53f164ad3ef6
Morgan Causey <morganelizabethcausey@gmail.com>
2023-07-27 20:59:57 -0400

    top and bottom text content

--- src/index.js
+++ src/index.js
@@ -55,6 +55,26 @@ midImg.setAttribute('src', siteContent.images['accent-img'])
 const footerLink = document.querySelector('footer a')
 footerLink.textContent = siteContent.footer.copyright
 footerLink.classList.add('bold')
+// contact
+const contact = document.querySelector('section.contact')
+contact.children[0].textContent = siteContent.contact['contact-h4']
+contact.children[1].textContent = siteContent.contact['address']
+contact.children[2].textContent = siteContent.contact['phone']
+contact.children[3].textContent = siteContent.contact['email']
+//top content
+const topContent = document.querySelector(' .top-content')
+topContent.children[0].children[0].textContent = siteContent['main-content']['features-h4']
+topContent.children[0].children[1].textContent = siteContent['main-content']['features-content']
+topContent.children[1].children[0].textContent = siteContent['main-content']['about-h4']
+topContent.children[1].children[1].textContent = siteContent['main-content']['about-content']
+//bottom content
+const bottomContent = document.querySelector(' .bottom-content')
+bottomContent.children[0].children[0].textContent = siteContent['main-content']['services-h4']
+bottomContent.children[0].children[1].textContent = siteContent['main-content']['services-content']
+bottomContent.children[1].children[0].textContent = siteContent['main-content']['product-h4']
+bottomContent.children[1].children[1].textContent = siteContent['main-content']['product-content']
+bottomContent.children[2].children[0].textContent = siteContent['main-content']['vision-h4']
+bottomContent.children[2].children[1].textContent = siteContent['main-content']['vision-content']
 
 //CLASS NAMES
 
